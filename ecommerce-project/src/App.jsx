b1f9103cbe1768c172fda45a439a89fdb2e19919
yeswanth pillimitla <yeswanthpillimitla@mail.com>
2026-01-23 22:51:21 +0530

    using route add oder and trackingPage

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,6 +1,8 @@
 import {Routes,Route} from 'react-router';
 import {HomePage} from './pages/HomePage';
 import {CheckoutPage}from './pages/CheckoutPage';
+import {OrderPage} from'./pages/OrderPage';
+import {TrackingPage} from './pages/TrackPage';
 import './App.css'
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage/>}/>
       <Route path="checkout" element={<CheckoutPage/>}/>
+      <Route path="orders" element={<OrderPage/>}/>
+      <Route path="tracking" element={<TrackingPage/>}/>
     </Routes>
     
   )
