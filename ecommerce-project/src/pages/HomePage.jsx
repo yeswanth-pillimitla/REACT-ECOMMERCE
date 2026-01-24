ec03afc38b34bf71c1c5072055b3e94187baf492
yeswanth pillimitla <yeswanthpillimitla@mail.com>
2026-01-24 10:45:25 +0530

    use backend product in ecommerce project

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,15 +1,22 @@
 import axios from 'axios';
+import {useEffect,useState} from 'react';
 import { Header } from '../components/Header';
-import { products } from '../../starting-code/data/products';
+// import { products } from '../../starting-code/data/products';
 import './HomePage.css';
 
 
 
 export function HomePage() {
-  axios.get('http://localhost:3000/api/products')
-  .then((response)=>{
-     console.log(response.data);
-  })
+ 
+ const [products,setProducts] =useState([]);
+
+  useEffect(()=>{
+    axios.get('http://localhost:3000/api/products')
+    .then((response)=>{
+      setProducts(response.data);
+    });
+  },[]);
+
   return (
     <>
       <title>Ecommerce Project</title>
