418c499b531eaaabb5a0b8be0cf4231619cd04da
yeswanth pillimitla <yeswanthpillimitla@mail.com>
2026-04-20 11:12:17 +0530

    header.tsx added

--- ecommerce-project-ts/src/components/Header.tsx
+++ ecommerce-project-ts/src/components/Header.tsx
@@ -5,39 +5,34 @@ import './header.css';
 interface HeaderProps {
   cart?: CartItem[];
 }
-export function Header({cart = []}:HeaderProps) {
-  let totalQuantity=0;
 
-  cart.forEach((cartItem)=>{
-      totalQuantity+=cartItem.quantity;
-  })
+export function Header({ cart = [] }: HeaderProps) {
+  // Use reduce for a cleaner, functional approach to summing quantities
+  const totalQuantity = cart.reduce((total, item) => total + item.quantity, 0);
+
   return (
     <div className="header">
       <div className="left-section">
         <Link to="/" className="header-link">
-          <img className="logo"
-            src="images/logo-white.png" />
-          <img className="mobile-logo"
-            src="images/mobile-logo-white.png" />
+          <img className="logo" src="images/logo-white.png" alt="Logo" />
+          <img className="mobile-logo" src="images/mobile-logo-white.png" alt="Mobile Logo" />
         </Link>
       </div>
 
       <div className="middle-section">
         <input className="search-bar" type="text" placeholder="Search" />
-
         <button className="search-button">
-          <img className="search-icon" src="images/icons/search-icon.png" />
+          <img className="search-icon" src="images/icons/search-icon.png" alt="Search" />
         </button>
       </div>
 
       <div className="right-section">
         <Link className="orders-link header-link" to="/orders">
-
           <span className="orders-text">Orders</span>
         </Link>
 
         <Link className="cart-link header-link" to="/checkout">
-          <img className="cart-icon" src="images/icons/cart-icon.png" />
+          <img className="cart-icon" src="images/icons/cart-icon.png" alt="Cart" />
           <div className="cart-quantity">{totalQuantity}</div>
           <div className="cart-text">Cart</div>
         </Link>
